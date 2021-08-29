006de8c8b4b497fee209c44123e921fbee6dea62
ben <s943536@gmail.com>
2021-08-30 00:39:07 +0800

    blog

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,13 +6,13 @@ from flask_login import LoginManager
 import subprocess
 from sqlalchemy.engine.create import create_engine
 
-final_db_url='postgresql+psycopg2:postgres://edgsqtentxumxq:5a8815f18889fcbd5c6755b4a6269ae826215773abb8940af9de4f684a12e371@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d20c58lko8m93n'
+final_db_url='postgresql+psycopg2://edgsqtentxumxq:5a8815f18889fcbd5c6755b4a6269ae826215773abb8940af9de4f684a12e371@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d20c58lko8m93n'
 engine = create_engine(final_db_url)
 
 
 app = Flask(__name__)  #create Flask project 
 app.config['SECRET_KEY']='8cea2065c057138b1193f0134c0f0c7e' #set up key to protect web application
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2:postgres://edgsqtentxumxq:5a8815f18889fcbd5c6755b4a6269ae826215773abb8940af9de4f684a12e371@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d20c58lko8m93n' #'postgresql+psycopg2://postgres:somerabbit@ec2-23-21-215-184.compute-1.amazonaws.com:5432/flaskblog?sslmode=require'   #set databse URI at sqlite  #'sqlite:///site.db' # 
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://edgsqtentxumxq:5a8815f18889fcbd5c6755b4a6269ae826215773abb8940af9de4f684a12e371@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d20c58lko8m93n' #'postgresql+psycopg2://postgres:somerabbit@ec2-23-21-215-184.compute-1.amazonaws.com:5432/flaskblog?sslmode=require'   #set databse URI at sqlite  #'sqlite:///site.db' # 
 
 
 
