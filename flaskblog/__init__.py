578cfa92df5775c41b29822e8c49e69959b725a1
ben <s943536@gmail.com>
2021-08-29 22:59:09 +0800

    blog

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,9 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+#DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/dbname"
+# sqlalchemy 1.4+ uses postgresql:// instead of postgres://
+#ssl_mode = "?sslmode=require"
+#DATABASE_URI += ssl_mode
+
 app = Flask(__name__)  #create Flask project 
 app.config['SECRET_KEY']='8cea2065c057138b1193f0134c0f0c7e' #set up key to protect web application
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://edgsqtentxumxq:5a8815f18889fcbd5c6755b4a6269ae826215773abb8940af9de4f684a12e371@ec2-34-228-100-83.compute-1.amazonaws.com:5432/d20c58lko8m93n'   #ec2-23-21-215-184.compute-1.amazonaws.com:5432/dd71doth8gopgh?sslmode=require' #set databse URI at sqlite  #'sqlite:///site.db' # 
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://postgres:somerabbit@localhost:5432/flaskblog?sslmode=require'   #ec2-23-21-215-184.compute-1.amazonaws.com:5432/dd71doth8gopgh?sslmode=require' #set databse URI at sqlite  #'sqlite:///site.db' # 
+
+
+
 db=SQLAlchemy(app) #set db by using SQLAlchemy class with input app
 bcrypt =Bcrypt(app)
 login_manager=LoginManager(app)
