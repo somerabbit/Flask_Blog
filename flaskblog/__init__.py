27fdddbae2d3587ca1ee9b0105b48e8ae61636c0
ben <s943536@gmail.com>
2021-08-29 22:37:54 +0800

    blog

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)  #create Flask project 
 app.config['SECRET_KEY']='8cea2065c057138b1193f0134c0f0c7e' #set up key to protect web application
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:somerabbit@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dd71doth8gopgh?sslmode=require"' #set databse URI at sqlite  #'sqlite:///site.db' # 
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://postgres:somerabbit@ec2-23-21-215-184.compute-1.amazonaws.com:5432/dd71doth8gopgh?sslmode=require' #set databse URI at sqlite  #'sqlite:///site.db' # 
 db=SQLAlchemy(app) #set db by using SQLAlchemy class with input app
 bcrypt =Bcrypt(app)
 login_manager=LoginManager(app)
