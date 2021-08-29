b3d6a78d902299ca3e081eb87f5b080235044969
ben <s943536@gmail.com>
2021-08-29 23:38:14 +0800

    blog

--- clock.py
+++ clock.py
@@ -2,8 +2,16 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour='5-1', minute='*/20')
+@sched.scheduled_job('cron', hour='6-1', minute='*/20')
 def scheduled_job():
+    print('========== APScheduler CRON =========')
+    # 馬上讓我們瞧瞧
+    print('This job runs 6am-1am every day */20 min.')
+    # 利用datetime查詢時間
+    print(f'{datetime.datetime.now().ctime()}')
+    print('========== APScheduler CRON =========')
+
+
     url = "https://ben-app-blog.herokuapp.com/home"
     conn = urllib.request.urlopen(url)
         
